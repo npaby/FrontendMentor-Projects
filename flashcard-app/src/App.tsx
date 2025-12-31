6e4dbaee6d13bf40e3613a5bc5cfb4d096060993
npaby <74232124+npaby@users.noreply.github.com>
2025-12-31 13:45:00 +0800

    Intiial Flashcard App

---
+++ flashcard-app/src/App.tsx
@@ -0,0 +1,62 @@
+
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			{/*<!-- Study Mode start -->*/}
+
+			<div>Study Mode</div>
+			<div>All Cards</div>
+
+			<div>All Categories</div>
+			<div>Hide Mastered</div>
+
+			<div>Shuffle</div>
+
+			{/*<!-- Populate card from JSON file -->*/}
+
+			<div>I Know This</div>
+			<div>Reset Progress</div>
+
+			<div>Previous</div>
+			<div>Next</div>
+
+			<div>Study Statistics</div>
+			<div>Totals Cards</div>
+			<div>Mastered</div>
+			<div>In Progress</div>
+			<div>Not Started</div>
+			{/*<!-- Study Mode end -->*/}
+
+			{/*<!-- All Cards start -->*/}
+
+			<div>Study Mode</div>
+			<div>All Cards</div>
+
+			<div>Question</div>
+			<div>e.g., "What is the capital of France?"</div>
+
+			<div>Answer</div>
+			<div>e.g., "Paris"</div>
+
+			<div>Category</div>
+			<div>e.g., "Geography"</div>
+
+			<div>Create Card</div>
+
+			<div>All Categories</div>
+			<div>Hide Mastered</div>
+
+			<div>Shuffle</div>
+
+			{/*<!-- Populate cards from JSON file -->*/}
+
+			{/*<!-- All Cards end -->*/}
+
+		</>
+	)
+}
+
+export default App
